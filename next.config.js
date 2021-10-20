735e18e3507a620b4a6a2a55f865b7f5c600ed3d
Jason Kennemer <jason@valescoagency.com>
2021-10-20 10:07:03 -0500

    Initial Next.js VA Boilerplate

---
+++ next.config.js
@@ -0,0 +1,100 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/**
+ * @type {import('next').NextConfig}
+ */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  experimental: {
+    esmExternals: true,
+    swcLoader: true,
+    swcMinify: false
+  },
+  poweredByHeader: false,
+  trailingSlash: true,
+  basePath: '',
+  // The starter code load resources from `public` folder with `router.basePath` in React components.
+  // So, the source code is "basePath-ready".
+  // You can remove `basePath` if you don't need it.
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'localhost', // Local Backend Server
+      'lh5.googleusercontent.com', // Google Content for profile pics
+      'pbs.twimg.com' // Twitter Profile Picture
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders
+      }
+    ];
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      });
+    }
+
+    return config;
+  }
+});
+
+// https://securityheaders.com
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.facebook.net *.twitter.com cdn.usefathom.com;
+  child-src *.youtube.com *.facebook.com *.facebook.net *.google.com *.twitter.com;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`;
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, '')
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  // Opt-out of Google FLoC: https://amifloced.org/
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+  }
+];
---
+++ tailwind.config.js
@@ -0,0 +1,154 @@
+const {
+  colors,
+  fontFamily,
+  screens,
+  spacing,
+} = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    screens: {
+      'xs': '480px',
+      '3xl': '1920px',
+      ...screens,
+    },
+    extend: {
+      animation: {
+        wiggle: 'wiggle 0.5s ease-in-out infinite',
+      },
+      colors: {
+        brand: {
+          DEFAULT: '#cb9866',
+          dark: '#be7e40',
+          light: '#d8b28c',
+          comp: '#6699cb',
+        },
+secondary: {
+          DEFAULT: '#1F243F',
+          '50': '#828CC1',
+          '100': '#717CB9',
+          '200': '#515FA6',
+          '300': '#414B83',
+          '400': '#303861',
+          '500': '#1F243F',
+          '600': '#0E101D',
+          '700': '#000000',
+          '800': '#000000',
+          '900': '#000000',
+        },
+        surface: {
+          dark: '#0f151d',
+          dl0: '#0f151d',
+          dl1: '#1b2128',
+          dl2: '#20252d',
+          dl3: '#22282f',
+          dl4: '#252a31',
+          dl5: '#292f36',
+          dl6: '#2c3138',
+          dl7: '#31363d',
+          dl8: '#33383f',
+          dl9: '#353a41',
+          light: 'colors.gray.600',
+        },
+        va: {
+          DEFAULT: '#cb9866',
+        },
+        gray: colors.coolGray,
+      },
+      fontFamily: {
+        sans: ['Nunito', ...fontFamily.sans],
+        headings: ['Montserrat', ...fontFamily.sans],
+      },
+      boxShadow: {
+        'lg-invert':
+          '0 10px 15px -3px rgba(255, 255, 255, 0.1), 0 4px 6px -2px rgba(255, 255, 255, 0.05)',
+        magical:
+          'rgba(0, 0, 0, 0.02) 0px 30px 30px, rgba(0, 0, 0, 0.03) 0px 0px 8px, rgba(0, 0, 0, 0.05) 0px 1px 0px',
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.700'),
+            a: {
+              color: theme('colors.blue.500'),
+              '&:hover': {
+                color: theme('colors.blue.700')
+              },
+              code: { color: theme('colors.blue.400') }
+            },
+            'h2,h3,h4': {
+              'scroll-margin-top': spacing[32]
+            },
+            code: { color: theme('colors.pink.500') },
+            'blockquote p:first-of-type::before': false,
+            'blockquote p:last-of-type::after': false
+          }
+        },
+        dark: {
+          css: {
+            color: theme('colors.gray.200'),
+            a: {
+              color: theme('colors.blue.400'),
+              '&:hover': {
+                color: theme('colors.blue.600')
+              },
+              code: { color: theme('colors.blue.400') }
+            },
+            blockquote: {
+              borderLeftColor: theme('colors.gray.700'),
+              color: theme('colors.gray.300')
+            },
+            'h2,h3,h4': {
+              color: theme('colors.gray.100'),
+              'scroll-margin-top': spacing[32]
+            },
+            hr: { borderColor: theme('colors.gray.700') },
+            ol: {
+              li: {
+                '&:before': { color: theme('colors.gray.500') }
+              }
+            },
+            ul: {
+              li: {
+                '&:before': { backgroundColor: theme('colors.gray.500') }
+              }
+            },
+            strong: { color: theme('colors.gray.100') },
+            thead: {
+              color: theme('colors.gray.100')
+            },
+            tbody: {
+              tr: {
+                borderBottomColor: theme('colors.gray.700')
+              }
+            }
+          }
+        }
+      }),
+      zIndex: {
+        '-10': '-10',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(4deg)' },
+        },
+      },
+    },
+  },
+  variants: {
+    typography: ['dark'],
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+  ],
+};
